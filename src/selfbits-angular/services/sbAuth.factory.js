9a03eedd758b346c81fd1cb375141090eb34e782
Oliver Kuppler <kreck@users.noreply.github.com>
2016-06-14 10:45:11 +0200

    update sbAuth to properly work on mobile devices with cordova

--- src/selfbits-angular/services/sbAuth.factory.js
+++ src/selfbits-angular/services/sbAuth.factory.js
@@ -1,87 +1,121 @@
 (function(angular) {
-	angular
-		.module('selfbitsAngular')
-		.factory('$sbAuth', sbAuth);
+    angular
+        .module('selfbitsAngular')
+        .factory('$sbAuth', sbAuth);
 
-	function sbAuth(sbConfig, sbState, $resource, $q, sbGuid, $http, $window, $interval) {
-		var sbAuth = {
-			social: social,
-			unlink: unlink,
-			login: login,
-			signup: signup,
-			logout: logout,
-			password: password,
-			isAuthenticated: isAuthenticated
-		};
+    function sbAuth(sbConfig, sbState, $resource, $q, sbGuid, $http, $window, $interval, $timeout) {
+        var sbAuth = {
+            social: social,
+            unlink: unlink,
+            login: login,
+            signup: signup,
+            logout: logout,
+            password: password,
+            isAuthenticated: isAuthenticated
+        };
 
-		function social(providerName) {
-			var deferred = new $q.defer();
-			var uniqueState = sbGuid.gen() + sbGuid.gen();
-			var authWindow = $window.open(sbConfig.domain + '/api/v1/oauth/' + providerName + '?sb_app_id=' + sbConfig.id + '&sb_app_secret=' + sbConfig.secret + '&state=' + uniqueState, '_blank', 'height=700, width=500');
-			var pingWindow = $interval(function() {
-				if (authWindow.closed) {
-					$http.get(sbConfig.domain + '/api/v1/oauth/' + providerName + '/token?sb_app_id=' + sbConfig.id + '&sb_app_secret=' + sbConfig.secret + '&state=' + uniqueState).then(function(res) {
-						deferred.resolve(res.data);
-						sbState.setToken(res.data.token);
-					}, function(err) {
-						deferred.reject(err);
-					});
-					$interval.cancel(pingWindow);
-				}
-			}, 1000);
-			return deferred.promise;
-		}
+        function social(providerName) {
+            var deferred = new $q.defer();
+            var uniqueState = sbGuid.gen() + sbGuid.gen();
+            var popupUrl = sbConfig.domain + '/api/v1/oauth/' + providerName + '?sb_app_id=' + sbConfig.id + '&sb_app_secret=' + sbConfig.secret + '&state=' + uniqueState;
+            var authWindow;
 
-		function unlink(providerName) {
-			var deferred = new $q.defer();
-			$http.delete(sbConfig.domain + '/api/v1/oauth/' + providerName + '/unlink').then(function(res) {
-				sbState.setToken(res.data.token);
-				deferred.resolve(res.data);
-			}, function(err) {
-				deferred.reject(err);
-			});
-			return deferred.promise;
-		}
+            if (window.cordova && window.cordova.InAppBrowser) {
+                authWindow = window.cordova.InAppBrowser.open(popupUrl, '_blank', 'location=no');
+                authWindow.addEventListener('loadstop', function(event) {
+                    if (event.url.indexOf(providerName + '/callback') > -1) {
+                        authWindow.close();
+                    }
+                });
+                authWindow.addEventListener('loaderror', function(event) {
+                    if (event.url.indexOf(providerName + '/callback') > -1) {
+                        authWindow.close();
+                    }
+                });
+                authWindow.addEventListener('exit', function() {
+                    $timeout(function() {
+                        getToken(providerName, uniqueState).then(function(res) {
+                            deferred.resolve(res.data);
+                            sbState.setToken(res.data.token);
+                        }, function(err) {
+                            deferred.reject(err);
+                        });
+                    }, 500);
+                });
+            } else {
+                authWindow = $window.open(popupUrl, '_blank', 'height=700, width=500');
+                var pingWindow = $interval(function() {
+                    if (authWindow.closed) {
+                        getToken(providerName, uniqueState).then(function(res) {
+                            deferred.resolve(res.data);
+                            sbState.setToken(res.data.token);
+                        }, function(err) {
+                            deferred.reject(err);
+                        });
+                        $interval.cancel(pingWindow);
+                    }
+                }, 1000);
+            }
+            return deferred.promise;
+        }
 
-		function password(newPassword, oldPassword) {
-			var deferred = new $q.defer();
-			$http.post(sbConfig.domain + '/api/v1/auth/password', { newPassword : newPassword, oldPassword : oldPassword}).then(function(res) {
-				deferred.resolve(res.data);
-			}, function(err) {
-				deferred.reject(err);
-			});
-			return deferred.promise;
-		}
+        function getToken(providerName, uniqueState) {
+            return $http.get(sbConfig.domain + '/api/v1/oauth/' + providerName + '/token?sb_app_id=' + sbConfig.id + '&sb_app_secret=' + sbConfig.secret + '&state=' + uniqueState);
+        }
 
-		function login(user) {
-			var deferred = new $q.defer();
-			$http.post(sbConfig.domain + '/api/v1/auth/login', user).then(function(res) {
-				sbState.setToken(res.data.token);
-				deferred.resolve(res.data);
-			}, function(err) {
-				deferred.reject(err);
-			});
-			return deferred.promise;
-		}
+        function unlink(providerName) {
+            var deferred = new $q.defer();
+            $http.delete(sbConfig.domain + '/api/v1/oauth/' + providerName + '/unlink').then(function(res) {
+                sbState.setToken(res.data.token);
+                deferred.resolve(res.data);
+            }, function(err) {
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        }
 
-		function signup(user) {
-			var deferred = new $q.defer();
-			$http.post(sbConfig.domain + '/api/v1/auth/signup', user).then(function(res) {
-				sbState.setToken(res.data.token);
-				deferred.resolve(res.data);
-			}, function(err) {
-				deferred.reject(err);
-			});
-			return deferred.promise;
-		}
+        function password(newPassword, oldPassword) {
+            var deferred = new $q.defer();
+            $http.post(sbConfig.domain + '/api/v1/auth/password', {
+                newPassword: newPassword,
+                oldPassword: oldPassword
+            }).then(function(res) {
+                deferred.resolve(res.data);
+            }, function(err) {
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        }
 
-		function logout() {
-			sbState.clear();
-		}
+        function login(user) {
+            var deferred = new $q.defer();
+            $http.post(sbConfig.domain + '/api/v1/auth/login', user).then(function(res) {
+                sbState.setToken(res.data.token);
+                deferred.resolve(res.data);
+            }, function(err) {
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        }
 
-		function isAuthenticated() {
-			return (sbState.getToken() !== null);
-		}
-		return sbAuth;
-	}
+        function signup(user) {
+            var deferred = new $q.defer();
+            $http.post(sbConfig.domain + '/api/v1/auth/signup', user).then(function(res) {
+                sbState.setToken(res.data.token);
+                deferred.resolve(res.data);
+            }, function(err) {
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        }
+
+        function logout() {
+            sbState.clear();
+        }
+
+        function isAuthenticated() {
+            return (sbState.getToken() !== null);
+        }
+        return sbAuth;
+    }
 })(angular);
